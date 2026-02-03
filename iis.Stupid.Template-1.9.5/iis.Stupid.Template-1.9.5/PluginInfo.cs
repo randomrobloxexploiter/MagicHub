a49b17b13ffcf1564d08ff7b3491287f87b0481b
NotMagicHub <ItzzzzMagicccc@Gmail.com>
2026-02-03 14:32:23 -0500

    Update PluginInfo.cs

--- iis.Stupid.Template-1.9.5/iis.Stupid.Template-1.9.5/PluginInfo.cs
+++ iis.Stupid.Template-1.9.5/iis.Stupid.Template-1.9.5/PluginInfo.cs
@@ -2,7 +2,7 @@
 {
     public class PluginInfo
     {
-        public const string GUID = "org.iidk.gorillatag.menutemplate";
+        public const string GUID = "org.Magic.gorillatag.menutemplate";
         public const string Name = "MagicHub";
         public const string Description = "made by MagicHub with all the love you can get <3 ";
         public const string Version = "1.9.5";
